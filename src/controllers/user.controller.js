38549ae6caf2c26de25e9c759fa642f99fc1d5a1
Latiful Rahman <latif.fiverr@gmail.com>
2025-09-06 15:16:05 +0000

    working auths routes

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -9,8 +9,6 @@ const registerUser = asynchandler(async (req, res) => {
 
     //get user details form frontend
     const { fullName, userName, email, password, } = req.body
-    // console.log(req.body)
-    console.log(fullName, userName, email, password);
 
     //validate user details
     if (
@@ -53,12 +51,9 @@ const registerUser = asynchandler(async (req, res) => {
         email,
         password,
         avatar: avatar.url,
-        coverImage: coverImage?.url || "", 
-       
+        coverImage: coverImage?.url || "",
+
     })
-
-   
-
 
     //remove password and refresh token from response
     //check of user created successfully 
@@ -72,4 +67,80 @@ const registerUser = asynchandler(async (req, res) => {
 
 })
 
-export { registerUser }
+const loginUser = asynchandler(async (req, res) => {
+    //get user details
+    const { userName, email, password } = req.body
+    //validate user details
+    if ([userName || email, password].some((field) => field?.trim() === "")) {
+        throw new ApiError(400, "All fields are required")
+    }
+
+    //check if user exists
+    const user = await User.findOne({ $or: [{ userName: userName?.toLowerCase() }, { email }] })
+    if (!user) {
+        throw new ApiError(404, "User not found")
+    }
+
+    //match password
+    const isMatch = await user.isPasswordCorrect(password)
+    if (!isMatch) {
+        throw new ApiError(401, "Invalid credentials")
+    }
+
+    //generate token
+    const accessToken = await user.generateAccessToken()
+    const refreshToken = await user.generateRefreshToken()
+
+    //save refresh token in db
+    user.refreshToken = refreshToken
+    await user.save({ validateBeforeSave: false })
+
+    //remove password and refresh token from response
+    const userData = await User.findById(user._id).select("-password -refreshToken -__v -createdAt -updatedAt")
+
+    //set cookies
+    const options = {
+        httpOnly: true,
+        secure: true,
+        sameSite: "strict",
+        maxAge: 7 * 24 * 60 * 60 * 1000 //7 days
+    }
+
+
+
+    //send response
+    return res
+        .status(200)
+        .cookie("accessToken", accessToken, options)
+        .cookie("refreshToken", refreshToken, options)
+        .json(new ApiResponse(200, { user: userData, accessToken, refreshToken }, "user logged in successfully"))
+})
+
+const logoutUser = asynchandler(async (req, res) => {
+    //get user id from req
+
+    const userId = req.user._id
+
+    //find user in db and remove refresh token
+    const user = await User.findById(userId)
+    if (!user) throw new ApiError(404, "User not found")
+
+    user.refreshToken = null
+    await user.save({ validateBeforeSave: false })
+
+    //remove cookies
+    const options = {
+        httpOnly: true,
+        secure: true,
+        sameSite: "strict",
+    }
+
+    res.clearCookie("accessToken", options)
+    res.clearCookie("refreshToken", options)
+
+    //send response
+    return res.status(200).json(new ApiResponse(200, null, "user logged out successfully"))
+
+})
+
+export { registerUser, loginUser, logoutUser }
---
+++ src/middlewares/auth.middleware.js
@@ -0,0 +1,23 @@
+//create auth middleware to protect private routes
+
+import jwt from "jsonwebtoken";
+import { asynchandler } from "../utils/asynchandler.js";
+
+
+export const verifyJWT = (req, res, next) => {
+    const token = req.cookies?.accessToken || req.headers?.authorization?.split(" ")[1];
+
+    if (!token) {
+        return res.status(401).json({ message: "Unauthorized" });
+    }
+
+
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err) {
+            return res.status(401).json({ message: "Unauthorized" });
+        }
+        req.user = decoded;
+        next();
+    })
+
+}
